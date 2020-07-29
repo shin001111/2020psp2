5e7367d723b3823fdb8351758d2621d3b90c3685
shin001111 <zin72384@gmail.com>
2020-07-30 01:49:51 +0900

    k01更新

--- k01/k01.c
+++ k01/k01.c
@@ -3,19 +3,18 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave);
-extern double var_online(double val,double ave,double ave2);
-int nr = 1;
-double ave= 0;
-double ave_n1 = 0;
-double ave2 = 0;
-double ave2_n1=0;
-double var = 0;
-double populationmean;
-double populationvar;
-double val;
+extern double ave_online(double val,double ave,int nr);
+extern double var_online(double ave,double ave2);
+
 int main(void)
-{
+{   int nr = 1;
+    double ave= 0;
+    double ave_n1 = 0;
+    double ave2 = 0;
+    double ave2_n1=0;
+    double var = 0;
+    double populationmean;
+    double populationvar;
     double val;
     char fname[FILENAME_MAX];
     char buf[256];
@@ -35,9 +34,9 @@ int main(void)
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
 
-        ave = ave_online(val,ave_n1);
-        ave2 = ave_online(val*val,ave2_n1);
-        var = var_online(val,ave,ave2_n1);
+        ave = ave_online(val,ave_n1,nr);
+        ave2 = ave_online(val*val,ave2_n1,nr);
+        var = var_online(ave,ave2);
         nr++;
         ave_n1=ave;
         ave2_n1=ave2;
@@ -60,12 +59,12 @@ printf("%lf\n",populationvar);
 
 }
 
-double ave_online(double val,double ave_n1)
+double ave_online(double val,double ave_n1,int nr)
 {
 return  ( ( ( nr - 1 ) * ave_n1 ) / nr ) + ( val / nr );
 }
 
-double var_online(double val,double ave_n1,double ave2_n1)
+double var_online(double ave_n1,double ave2)
 {
-return (ave_online(val*val, ave2_n1)-ave_n1*ave_n1);
+return (ave2-ave_n1*ave_n1);
 }
